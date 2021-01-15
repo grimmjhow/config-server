f50d74ae3d067d76da0d45a4b02b0f28af9c6e89
grimmjhow <felipe.cife@gmail.com>
2021-01-15 08:02:34 -0300

    add project files

---
+++ src/test/java/br/com/spring/config/server/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
